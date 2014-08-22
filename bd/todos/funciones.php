347c809c48355b3e8f75dd7e1789772a8ed48454
Usuario <Usuario@Usuario-PC>
2014-08-22 10:55:22 -0300

    Jueves 21/08

--- bd/todos/funciones.php
+++ bd/todos/funciones.php
@@ -2,21 +2,24 @@
 Aqui se detallan todas las funciones que pueden ser utilizadas por todos los usuarios
 incluso los invitados (NO REGISTRADOS)
 Se priorizan funciones de listado y busqueda
-
 -->
-
 <?php
+
 function listarInvestigadores() {
     include "bd/conexion.php";
     $sql = "select * from Usuario order by Apellido";
     $consulta = $cnn->prepare($sql);
     $param = array(0);
     if ($consulta->execute($param)) {
-        //echo "<br>consulta ejecutada...<br>";
+        echo "<table border = 1>";
+        echo "<tr><th>ID</th><th>Apellido</th><th>Nombre</th><th>EMail</th></tr>";
+        while ($registro = $consulta->fetch()) {
+            echo "<tr><td>$registro[idUsuario]</td><td>" . utf8_encode($registro[Apellido]) . "</td><td>" . utf8_encode($registro[Nombre]) . "</td><td>" . utf8_encode($registro[EMail]) . "</td></tr><br>";
+        }
+        echo "</table>";
     } else {
         //print_r($consulta->errorInfo());
     }
-    return $consulta;
+    //return $consulta;
 }
-
 ?>
