764371faff5539371ce690c6992d1285594fca60
Usuario <Usuario@Usuario-PC>
2014-08-26 12:13:08 -0300

    Jueves 21/08

--- formulario/form_busquedageneral.php
+++ formulario/form_busquedageneral.php
@@ -1,30 +1,31 @@
-<?php include "funciones_formulario.php"?>
+<?php include "funciones_formulario.php" ?>
 <legend>Búsqueda</legend>
-<form name="frmBusqueda" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+<form> <!--name="frmBusqueda" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"-->
     <p>
-        <input type="radio" name="buscar" id="buscar" value="1" onclick="formulario_busqueda(this)"/> Proyecto  
-        <input type="radio" name="buscar" id="buscar" value="2" onclick="formulario_busqueda(this)"/> Programa
-        <input type="radio" name="buscar" id="buscar" value="3" onclick="formulario_busqueda(this)"/> Publicación
-        <input type="radio" name="buscar" id="buscar" value="4" onclick="formulario_busqueda(this)"/> Usuario
+        <input type="radio" name="buscar" id="buscar" value="1" onclick="<?php formulario_busqueda($id)?>" cheked="cheked"/> Proyecto  
+        <input type="radio" name="buscar" id="buscar" value="2" onclick="<?php formulario_busqueda($id)?>"/> Programa
+        <input type="radio" name="buscar" id="buscar" value="3" onclick="<?php formulario_busqueda($id)?>"/> Publicación
+        <input type="radio" name="buscar" id="buscar" value="4" onclick="<?php formulario_busqueda($id)?>"/> Usuario
     </p>
-    <div id="div_proyecto" style="display: none">
-        <label>Título </label> <input type="text" name = "titulo"/>
-        <label>Disciplina </label> <input type="text" name = "disciplina"/>
-        <label>Palabras Clave </label> <input type="text" name = "clave"/>
-        <label>Año </label> <input type="text" name = "año"/>
+
+    <div id="div_proyecto" style = "display: block;" >
+        <label > Título Proyecto </label> <input type="text" name = "titulo"/><br>
+        <label > Disciplina </label> <input type="text" name = "disciplina"/><br>
+        <label > Palabras Clave </label> <input type="text" name = "clave"/><br>
+        <label > Año </label> <input type="text" name = "año"/><br>
     </div>
-    <div id="div_programa" style="display: none">
-        <label>Título </label> <input type="text" name = "titulo"/>
-        <label>Disciplina </label> <input type="text" name = "disciplina"/>
-        <label>Palabras Clave </label> <input type="text" name = "clave"/>
-        <label>Año </label> <input type="text" name = "año"/>
+    <div id = "div_programa" style = "display: none;" >
+        <label > Título Programa </label> <input type="text" name = "titulo"/><br>
+        <label > Disciplina </label> <input type="text" name = "disciplina"/><br>
+        <label > Palabras Clave </label> <input type="text" name = "clave"/><br>
+        <label > Año </label> <input type="text" name = "año"/><br>
     </div>
-    <div id="div_publicacion" style="display: none">
-        <label>Título </label> <input type="text" name = "titulo"/>
-        <label>Año </label> <input type="text" name = "año"/>
+    <div id = "div_publicacion" style = "display: none;" >
+        <label > Título Publicación </label> <input type="text" name = "titulo"/><br>
+        <label > Año </label> <input type="text" name = "año"/><br>
     </div>
-    <div id="div_usuario" style="display: none">
-        
+    <div id = "div_usuario" style = "display: none;" >
+        <label > Nombre </label> <input type="text" name = "nombre"/><br>
+        <label > Apellido </label> <input type="text" name = "apellido"/><br>
     </div>
-        
 </form>
--- formulario/funciones_formulario.php
+++ formulario/funciones_formulario.php
@@ -2,29 +2,29 @@
 
 function formulario_busqueda($elemento) {
 
-    switch ($elemento . value) {
-        case 1:
-            document . getElementById("div_proyecto") . style . display("block");
-            document . getElementById("div_programa") . style . display("none");
-            document . getElementById("div_publicacion") . style . display("none");
-            document . getElementById("div_usuario") . style . display("none");
+    switch ($elemento) {
+        case "1":
+            document.getElementById("div_proyecto").style.display("block");
+            document.getElementById("div_programa").style.display("none");
+            document.getElementById("div_publicacion").style.display("none");
+            document.getElementById("div_usuario").style.display(none);
             break;
 
-        case 2:
-            document . getElementById("div_proyecto") . style . display("none");
-            document . getElementById("div_programa") . style . display("block");
-            document . getElementById("div_publicacion") . style . display("none");
-            document . getElementById("div_usuario") . style . display("none");
+        case "2":
+            document.getElementById("div_proyecto").style.display(none);
+            document.getElementById("div_programa").style.display(block);
+            document.getElementById("div_publicacion").style.display(none);
+            document.getElementById("div_usuario").style.display(none);
             break;
 
-        case 3:
+        case "3":
             document . getElementById("div_proyecto") . style . display("none");
             document . getElementById("div_programa") . style . display("none");
             document . getElementById("div_publicacion") . style . display("block");
             document . getElementById("div_usuario") . style . display("none");
             break;
 
-        case 4:
+        case "4":
             document . getElementById("div_proyecto") . style . display("none");
             document . getElementById("div_programa") . style . display("none");
             document . getElementById("div_publicacion") . style . display("none");
