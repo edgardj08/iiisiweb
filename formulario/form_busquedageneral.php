7ec618b7826577eea2d4fcbfd0a61c9d2f35c63e
Usuario <Usuario@Usuario-PC>
2014-09-08 15:53:39 -0300

    Jueves 21/08

--- formulario/form_busquedageneral.php
+++ formulario/form_busquedageneral.php
@@ -1,31 +1,47 @@
+<!-- 
+Muestra un RADIO BUTTON donde se elige que es lo que se desea buscar.
+Se habilitara un formulario con campos adecuados para completar, segun
+la seleccion del usuario.
+-->
+
 <?php include "funciones_formulario.php" ?>
 <legend>Búsqueda</legend>
-<form> <!--name="frmBusqueda" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"-->
+<form name="frmBusqueda" action="" method="post">
     <p>
-        <input type="radio" name="buscar" id="buscar" value="1" onclick="<?php formulario_busqueda($id)?>" cheked="cheked"/> Proyecto  
-        <input type="radio" name="buscar" id="buscar" value="2" onclick="<?php formulario_busqueda($id)?>"/> Programa
-        <input type="radio" name="buscar" id="buscar" value="3" onclick="<?php formulario_busqueda($id)?>"/> Publicación
-        <input type="radio" name="buscar" id="buscar" value="4" onclick="<?php formulario_busqueda($id)?>"/> Usuario
+        <input type="radio" name="buscar" id="proyecto" value="1" onclick="formulario_busqueda();"/> Proyecto  
+        <input type="radio" name="buscar" id="programa" value="2" onclick="formulario_busqueda();"/> Programa
+        <input type="radio" name="buscar" id="publicacion" value="3" onclick="formulario_busqueda();"/> Publicación
+        <input type="radio" name="buscar" id="usuario" value="4" onclick="formulario_busqueda();"/> Usuario
+        <input type = "submit" value ="Buscar"/>
     </p>
 
-    <div id="div_proyecto" style = "display: block;" >
-        <label > Título Proyecto </label> <input type="text" name = "titulo"/><br>
-        <label > Disciplina </label> <input type="text" name = "disciplina"/><br>
-        <label > Palabras Clave </label> <input type="text" name = "clave"/><br>
-        <label > Año </label> <input type="text" name = "año"/><br>
+    <div id="div_proyecto" style = "display: none;" >
+        <table>
+            <tr><td>Título Proyecto</td><td> <input type="text" name = "titulo"/><br></td>  </tr>
+            <tr><td>Disciplina</td><td> <input type="text" name = "disciplina"/><br></td>  </tr>
+            <tr><td>Palabras Clave</td><td> <input type="text" name = "clave"/><br></td>  </tr>
+            <tr><td>Año</td><td> <input type="text" name = "año"/><br></td>  </tr>
+        </table>
+
     </div>
     <div id = "div_programa" style = "display: none;" >
-        <label > Título Programa </label> <input type="text" name = "titulo"/><br>
-        <label > Disciplina </label> <input type="text" name = "disciplina"/><br>
-        <label > Palabras Clave </label> <input type="text" name = "clave"/><br>
-        <label > Año </label> <input type="text" name = "año"/><br>
+        <table>
+            <tr><td>Título Programa</td><td> <input type="text" name = "titulo"/><br></td>  </tr>
+            <tr><td>Disciplina</td><td> <input type="text" name = "disciplina"/><br></td>  </tr>
+            <tr><td>Palabras Clave</td><td> <input type="text" name = "clave"/><br></td>  </tr>
+            <tr><td>Año</td><td> <input type="text" name = "año"/><br></td>  </tr>
+        </table>
     </div>
     <div id = "div_publicacion" style = "display: none;" >
-        <label > Título Publicación </label> <input type="text" name = "titulo"/><br>
-        <label > Año </label> <input type="text" name = "año"/><br>
+        <table>
+            <tr><td>Título Publicación</td><td> <input type="text" name = "titulo"/><br></td>  </tr>
+            <tr><td>Año</td><td> <input type="text" name = "año"/><br></td>  </tr>
+        </table>
     </div>
     <div id = "div_usuario" style = "display: none;" >
-        <label > Nombre </label> <input type="text" name = "nombre"/><br>
-        <label > Apellido </label> <input type="text" name = "apellido"/><br>
+        <table>
+            <tr><td>Nombre</td><td> <input type="text" name = "nombre"/><br></td>  </tr>
+            <tr><td>Apellido</td><td> <input type="text" name = "apellido"/><br></td>  </tr>
+        </table>
     </div>
 </form>
--- formulario/funciones_formulario.php
+++ formulario/funciones_formulario.php
@@ -1,38 +1,41 @@
-<?php
+<!-- 
+Evalua la seleccion del Radio Button del archivo form_busquedageneral.php
+y muestra el formulario adecuado, ocultando los demas
+-->
 
-function formulario_busqueda($elemento) {
+<script type="text/javascript">
+    function formulario_busqueda() {
+        switch (document.frmBusqueda.buscar.value) {
+            case "1":
+                document.getElementById('div_proyecto').style.display = 'block';
+                document.getElementById('div_programa').style.display = 'none';
+                document.getElementById('div_publicacion').style.display = 'none';
+                document.getElementById('div_usuario').style.display = 'none';
+                break;
+            case "2":
+                document.getElementById('div_proyecto').style.display = 'none';
+                document.getElementById('div_programa').style.display = 'block';
+                document.getElementById('div_publicacion').style.display = 'none';
+                document.getElementById('div_usuario').style.display = 'none';
+                break;
 
-    switch ($elemento) {
-        case "1":
-            document.getElementById("div_proyecto").style.display("block");
-            document.getElementById("div_programa").style.display("none");
-            document.getElementById("div_publicacion").style.display("none");
-            document.getElementById("div_usuario").style.display(none);
-            break;
+            case "3":
+                document.getElementById('div_proyecto').style.display = 'none';
+                document.getElementById('div_programa').style.display = 'none';
+                document.getElementById('div_publicacion').style.display = 'block';
+                document.getElementById('div_usuario').style.display = 'none';
+                break;
 
-        case "2":
-            document.getElementById("div_proyecto").style.display(none);
-            document.getElementById("div_programa").style.display(block);
-            document.getElementById("div_publicacion").style.display(none);
-            document.getElementById("div_usuario").style.display(none);
-            break;
+            case "4":
+                document.getElementById('div_proyecto').style.display = 'none';
+                document.getElementById('div_programa').style.display = 'none';
+                document.getElementById('div_publicacion').style.display = 'none';
+                document.getElementById('div_usuario').style.display = 'block';
+                break;
+            default:
+                break;
 
-        case "3":
-            document . getElementById("div_proyecto") . style . display("none");
-            document . getElementById("div_programa") . style . display("none");
-            document . getElementById("div_publicacion") . style . display("block");
-            document . getElementById("div_usuario") . style . display("none");
-            break;
-
-        case "4":
-            document . getElementById("div_proyecto") . style . display("none");
-            document . getElementById("div_programa") . style . display("none");
-            document . getElementById("div_publicacion") . style . display("none");
-            document . getElementById("div_usuario") . style . display("block");
-            break;
-        default:
-            break;
+        }
     }
-}
 
-?>
+</script>
